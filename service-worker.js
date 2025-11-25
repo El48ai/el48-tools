0302428b140e71ab24fecb1e9f501d4bfa851151
ELPADRI <elpadri91@gmail.com>
2025-11-25 22:52:50 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('el48-cache-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './compress.html',
+        './favicon.ico',
+        './manifest.webmanifest',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
