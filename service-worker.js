efa9a52dce94b6f6f9f78e0c05829fcbeca363ee
ELPADRI <elpadri91@gmail.com>
2026-06-05 12:05:36 +0700

    Update service worker to use new cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,40 @@
+const CACHE = 'el48-cache-v2';
+
+const FILES = [
+  './',
+  './index.html',
+  './compress.html',
+  './enhance.html',
+  './privacy.html',
+  './terms.html',
+  './favicon.ico',
+  './manifest.webmanifest',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-512.png',
+  './og-image.png',
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('el48-cache-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './compress.html',
-        './favicon.ico',
-        './manifest.webmanifest',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
